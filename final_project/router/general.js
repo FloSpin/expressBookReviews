aa43384bdbdf3cf003b71edc305f30ed917a486a
flospin <florianspinoza@gmail.com>
2024-10-16 07:29:20 -0400

    get by author and title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,20 +18,43 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
+
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;  // Get the author from request parameters
+    const bookKeys = Object.keys(books);  // Get all book keys
+    
+    const matchingBooks = bookKeys
+        .map(key => books[key])  // Map keys to book objects
+        .filter(book => book.author === author);  // Filter books by author
+
+    if (matchingBooks.length > 0) {
+        res.status(200).json(matchingBooks);  // Return matching books if found
+    } else {
+        res.status(404).json({ message: "No books found for this author" });  // Handle no matches
+    }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;  // Get the author from request parameters
+    const bookKeys = Object.keys(books);  // Get all book keys
+    
+    const matchingBooks = bookKeys
+        .map(key => books[key])  // Map keys to book objects
+        .filter(book => book.title === title);  // Filter books by author
+
+    if (matchingBooks.length > 0) {
+        res.status(200).json(matchingBooks);  // Return matching books if found
+    } else {
+        res.status(404).json({ message: "No books found for this title" });  // Handle no matches
+    }
 });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
